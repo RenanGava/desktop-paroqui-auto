b1194404d2606f68fae7c7e58a13449ac07bbfba
RenanGava <renandellecrodegava7040@gmail.com>
2026-07-10 19:31:57 -0300

    local changes

--- src/hooks/useConfigApp/useConfigColeta/index.tsx
+++ src/hooks/useConfigApp/useConfigColeta/index.tsx
@@ -42,7 +42,7 @@ export function useConfigColetaApp() {
       setIsLoading(false)
     }
     handleCompareColetasDB();
-  }, []);
+  }, [])
 
   async function handleSyncAllColetasDB() {
     setIsLoading(true)
